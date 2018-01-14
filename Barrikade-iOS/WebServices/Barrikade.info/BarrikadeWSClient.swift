5a3b227b0d0c558c96f220f67a30048109a16f06
julien.cloupet@niji.fr <julien.cloupet@niji.fr>
2018-01-14 19:19:33 +0100

    Add Barrikade.info WS client

---
+++ Barrikade-iOS/WebServices/Barrikade.info/BarrikadeWSClient.swift
@@ -0,0 +1,39 @@
+//
+//  BarrikadeWSClient.swift
+//  Barrikade-iOS
+//
+//  Created by Pr0gmaT1K on 02/01/2018.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import RxSwift
+import Alamofire
+import NetworkStack
+
+final class BarrikadeWSClient {
+  // MARK: - Properties
+  private static let baseURL = "https://barrikade.info/"
+  private static let appName = "barrikade_info"
+  private static let keychainService = KeychainService(serviceType: BarrikadeWSClient.baseURL)
+  private static let networkStack = NetworkStack(baseURL: BarrikadeWSClient.appName, keychainService: keychainService)
+
+
+  // Mark:- Services
+}
+
